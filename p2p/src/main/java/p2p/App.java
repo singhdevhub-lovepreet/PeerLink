fbd0e5d409137a627da4c8c6b7fa1190aa087d3c
lovepreet-singh_dtpl <lovepreet.singh@cred.club>
2025-05-18 18:09:20 +0530

    Added socket programming concepts

--- p2p/src/main/java/p2p/App.java
+++
@@ -1,7 +0,0 @@
-package p2p;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/test/java/p2p/AppTest.java
+++ src/test/java/p2p/AppTest.java
@@ -3,6 +3,8 @@ package p2p;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import p2p.service.FileSharer;
+import p2p.service.RecieverServer;
 
 /**
  * Unit test for simple App.
@@ -12,8 +14,23 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
+//    private void recieveFile(Integer inviteCode){
+//        RecieverServer recieverServer = new RecieverServer();
+//        try{
+//            recieverServer.receiveFile("127.0.0.1", inviteCode, "test1.txt");
+//        }catch (Exception ex){
+//            System.err.println("File couldn't get");
+//            // ignored
+//        }
+//    }
+
     @Test
     public void shouldAnswerWithTrue() {
+//        FileSharer fileSharer = new FileSharer();
+//        int inviteCode = fileSharer.offerFile("/Users/lovepreetsingh/Desktop/PeerLink/src/test/java/p2p/test.txt");
+//        System.out.println("port generated " + inviteCode);
+//        fileSharer.startFileServer(inviteCode);
+//        Thread thread = new Thread(() -> recieveFile(inviteCode));
         assertTrue(true);
     }
 }
